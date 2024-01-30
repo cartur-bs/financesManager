62ab2eda65459c7cde365bda2fc8f425899481af
cartur-bs <cartur758@gmail.com>
2024-01-30 15:11:22 -0300

    feat: main class implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import org.example.entities.Purchase;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Would you like to add a new purchase or consult one?(add/consult)");
+
+        String responseAction = sc.next().toLowerCase();
+        if (responseAction.equals("add")) {
+            System.out.println("Insert purchase date(dd/MM/yyyy)");
+            String purchaseDateScanner = sc.next();
+            LocalDate formatedDate = LocalDate.parse(purchaseDateScanner, dateFormat);
+            if (formatedDate.isAfter(LocalDate.now())) {
+                System.out.println("Please, insert a date past or equal today!");
+                return;
+            }
+            System.out.println("Insert the class of your purchase:");
+            String purchaseClassScanner = sc.next();
+            System.out.println("Insert the value of your purchase");
+            double purchaseValueScanner = sc.nextDouble();
+
+            try {
+                Purchase newPurchase = new Purchase(null, formatedDate, purchaseClassScanner, purchaseValueScanner);
+                newPurchase.insertPurchase(newPurchase);
+            } catch (InputMismatchException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
