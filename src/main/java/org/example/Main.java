116c718a9235d086acff6fab227329eea5f1efa8
cartur-bs <cartur758@gmail.com>
2024-02-05 15:26:55 -0300

    gettingUser() created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,17 @@ package org.example;
 import org.example.entities.Purchase;
 import org.example.entities.User;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         System.out.println("Hello, would you like to create a new profile or log in?(create/log in)");
         String responseActionAccess = sc.nextLine().toLowerCase();
@@ -30,59 +32,76 @@ public class Main {
                 User.createUser(newUser);
             }
         }
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.println("Would you like to add a new purchase or consult one?(add/consult)");
-        String responseAction = sc.next().toLowerCase();
+        if (responseActionAccess.equals("log in")) {
+            System.out.println("Please, insert your username");
+            String userNameScanner = sc.next();
+            System.out.println("Insert your password");
+            String passwordScannerAccess = sc.next();
 
-        if (responseAction.equals("add")) {
-            try {
-                System.out.println("Insert purchase date(dd/MM/yyyy)");
-                String purchaseDateScanner = sc.next();
-                LocalDate formattedDate = LocalDate.parse(purchaseDateScanner, dateFormat);
-                //verifies that the user doesn't insert a date passed today, only past dates
-                if (formattedDate.isAfter(LocalDate.now())) {
-                    System.out.println("Please, insert a date past or equal today!");
-                    return;
-                }
-                System.out.println("Insert the class of your purchase:(ex.: food, health, home)");
-                String purchaseClassScanner = sc.next();
-                System.out.println("Insert the value of your purchase(ex.: 10.99)");
-                double purchaseValueScanner = sc.nextDouble();
-                Purchase newPurchase = new Purchase(null, formattedDate, purchaseClassScanner, purchaseValueScanner);
-                newPurchase.insertPurchase(newPurchase);
-                System.out.println("Insertion completed successfully!");
-                return; //exits the function and doesn't display the else
-            } catch (InputMismatchException e) {
-                System.out.println("Insert a valid value");
-                e.printStackTrace();
-            }
-        }
-
-        if (responseAction.equals("consult")) {
-            System.out.println("Consult by class or date?(class/date)");
-            String consultResponse = sc.next().toLowerCase();
-            if (consultResponse.equals("class")) {
-                System.out.println("Insert purchase class");
-                String classPurchase = sc.next();
-                Purchase.getPurchaseByClass(classPurchase);
+            if (!User.gettingUser(userNameScanner, passwordScannerAccess)) {
+                System.out.println("Wrong user name or password");
                 return;
             }
-            if (consultResponse.equals("date")) {
-                System.out.println("Insert the date(dd/MM/yyyy)");
-                String consultDate = sc.next();
-                try {
-                    LocalDate consultDateFormatted = LocalDate.parse(consultDate, dateFormat);
-                    Purchase.getPurchaseByDate(consultDateFormatted);
-                } catch (DateTimeParseException e) {
-                    System.out.println("Insert a valid value");
-                    e.printStackTrace();
+
+            else {
+                System.out.println("Welcome, " + userNameScanner);
+
+                DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                System.out.println("Would you like to add a new purchase or consult one?(add/consult)");
+                String responseAction = sc.next().toLowerCase();
+
+                if (responseAction.equals("add")) {
+                    try {
+                        System.out.println("Insert purchase date(dd/MM/yyyy)");
+                        String purchaseDateScanner = sc.next();
+                        LocalDate formattedDate = LocalDate.parse(purchaseDateScanner, dateFormat);
+                        //verifies that the user doesn't insert a date passed today, only past dates
+                        if (formattedDate.isAfter(LocalDate.now())) {
+                            System.out.println("Please, insert a date past or equal today!");
+                            return;
+                        }
+                        System.out.println("Insert the class of your purchase:(ex.: food, health, home)");
+                        String purchaseClassScanner = sc.next();
+                        System.out.println("Insert the value of your purchase(ex.: 10.99)");
+                        double purchaseValueScanner = sc.nextDouble();
+                        Purchase newPurchase = new Purchase(null, formattedDate, purchaseClassScanner, purchaseValueScanner);
+                        newPurchase.insertPurchase(newPurchase);
+                        System.out.println("Insertion completed successfully!");
+                        return; //exits the function and doesn't display the else
+                    } catch (InputMismatchException e) {
+                        System.out.println("Insert a valid value");
+                        e.printStackTrace();
+                    }
                 }
-            } else {
-                System.out.println("Please, only insert valid inputs!");
+
+                if (responseAction.equals("consult")) {
+                    System.out.println("Consult by class or date?(class/date)");
+                    String consultResponse = sc.next().toLowerCase();
+                    if (consultResponse.equals("class")) {
+                        System.out.println("Insert purchase class");
+                        String classPurchase = sc.next();
+                        Purchase.getPurchaseByClass(classPurchase);
+                        return;
+                    }
+                    if (consultResponse.equals("date")) {
+                        System.out.println("Insert the date(dd/MM/yyyy)");
+                        String consultDate = sc.next();
+                        try {
+                            LocalDate consultDateFormatted = LocalDate.parse(consultDate, dateFormat);
+                            Purchase.getPurchaseByDate(consultDateFormatted);
+                        } catch (DateTimeParseException e) {
+                            System.out.println("Insert a valid value");
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println("Please, only insert valid inputs!");
+                    }
+                } else {
+                    System.out.println("Please, only insert the informed options!");
+                }
             }
-        } else {
-            System.out.println("Please, only insert the informed options!");
         }
+
 
         sc.close();
     }
--- src/main/java/org/example/entities/User.java
+++ src/main/java/org/example/entities/User.java
@@ -1,6 +1,11 @@
 package org.example.entities;
 
+import org.example.DB.DBProperties;
+
 import javax.persistence.*;
+
+import java.sql.*;
+
 
 @Entity
 public class User {
@@ -9,6 +14,7 @@ public class User {
     private int Id;
     private String userName;
     private String password;
+
 
     public User() {
     }
@@ -22,12 +28,47 @@ public class User {
         return userName;
     }
 
-    public static void createUser(User user){
+    public static boolean gettingUser(String userName, String password) throws SQLException {
+        Connection con = DBProperties.getConnection();
+        String sql = "SELECT * FROM User WHERE userName = '" + userName + "' AND password = '" + password + "'";
+        Statement st = con.createStatement();
+        ResultSet rst;
+
+        boolean teste = false;
+        try {
+            rst = st.executeQuery(sql);
+            while( rst.next()){
+               String userNameReturned = rst.getString("userName");
+               String passwordReturned = rst.getString("password");
+               int idR = rst.getInt("id");
+                System.out.println(userNameReturned + passwordReturned +idR);
+                teste = true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if(con != null){
+                con.close();
+            }
+        }
+        return teste;
+    }
+
+    public static void createUser(User user) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("expManager");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
         em.persist(user);
         em.getTransaction().commit();
         em.close();
+    }
+
+    public String toString() {
+        return "User{" +
+                "Id=" + Id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
